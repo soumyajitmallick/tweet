c0363b9ba967c7ae034088c9cff2a86385cca7b9
soumyajitmallick <soumyajitmallick0123@gmail.com>
2025-04-08 12:36:28 +0530

    new pages

---
+++ som/som/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for som project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'som.settings')
+
+application = get_asgi_application()
---
+++ som/tweet/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from .views import search_tweets
+
+urlpatterns = [
+    path('', views.tweet_list, name='tweet_list'),
+    path('create/', views.tweet_create, name='tweet_create'),
+    path('<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
+    path('<int:tweet_id>/delete/', views.tweet_delete, name='tweet_delete'),
+    path('register/', views.register, name='register'),
+    path('search/', search_tweets, name='tweet_search'),
+] 
